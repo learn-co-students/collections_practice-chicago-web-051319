9cc0a4f6d712baeb1da5709815b1796d4393b22a
Markus Sanders <markus.sanderst@gmail.com>
2019-05-14 13:51:17 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,46 @@
+def sort_array_asc(arr)
+  arr.sort { |a, b| a <=> b }
+end
 
+def sort_array_desc(arr)
+  arr.sort { |a, b| b <=> a }
+end
+
+def sort_array_char_count(arr)
+  arr.sort { |a, b| a.length <=> b.length }
+end
+
+def swap_elements(arr) 
+  sec = arr[1]
+  third = arr[2]
+  arr[1] = third
+  arr[2] = sec
+  arr
+end
+
+def reverse_array(arr)
+  arr.reverse
+end
+
+def kesha_maker(arr)
+ output = arr.each { |w| w[2] = '$'}
+ output
+end
+
+def find_a(array)
+  d.split('').first == 'a'}
+end
+
+def sum_array(arr)
+  sum = 0
+  arr.each do |num|
+    sum += num 
+  end
+  sum
+end
+
+def add_s(arr)
+  output = arr.map {|v| v + 's'}
+  output[1] = arr[1]
+  output
+end
